4fd2771953a8349426475e77f4a7ff032c332434
222darkdark <alexparaschiv@users.noreply.github.com>
2021-04-27 15:11:02 +0300

    Partea initiala

---
+++ PortalDocPat/PortalDocPat/Models/Article.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace PortalDocPat.Models
+{
+	public class Article
+	{
+		[Key]
+		public int ArticleId { get; set; }
+
+		[Required]
+		public string Title { get; set; }
+		[Required]
+		public string Content { get; set; }
+		[Required]
+		public DateTime Date { get; set; }
+
+		public int DoctorId { get; set; }
+		public virtual Doctor Doctor { get; set; }
+	}
+}
---
+++ PortalDocPat/PortalDocPat/Models/Specialization.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace PortalDocPat.Models
+{
+	public class Specialization
+	{
+		[Key]
+		public int SpecializationId { get; set; }
+
+		[Required]
+		public string SpecializationName { get; set; }
+		[Required]
+		public float Price { get; set; }
+
+		public virtual ICollection<Doctor> Doctors { get; set; }
+	}
+}
