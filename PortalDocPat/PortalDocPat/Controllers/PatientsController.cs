1f1d583678cfe8cd49f4d45e529a0a390704d1d4
222darkdark <alexparaschiv@users.noreply.github.com>
2021-05-19 01:40:23 +0300

    new controllers

---
+++ PortalDocPat/PortalDocPat/Controllers/PatientsController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PortalDocPat.Controllers
+{
+    public class PatientsController : Controller
+    {
+        // GET: Patients
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
