19b3d3a0701265619ccd95fe571e614904b199e9
StaicuBogdan <61222625+StaicuBogdan@users.noreply.github.com>
2021-06-14 23:07:04 +0300

    ok

--- PortalDocPat/PortalDocPat/App_Start/RouteConfig.cs
+++ PortalDocPat/PortalDocPat/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace PortalDocPat
 			routes.MapRoute(
 				name: "Default",
 				url: "{controller}/{action}/{id}",
-				defaults: new { controller = "Home", action = "About", id = UrlParameter.Optional }
+				defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
 			);
 		}
 	}
